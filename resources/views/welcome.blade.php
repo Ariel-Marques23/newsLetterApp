f7ad1ac593718c9b2cdcea902c2ec7e57d0bde83
Ariel-Marques23 <arielmarques789@gmail.com>
2023-08-30 22:27:31 -0300

    visualizaçõa historico de mensagem

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,20 +8,44 @@
         <title>News letter front</title>
     </head>
         <body class="antialiased">
-
-                <div class="center">
-                    <div>
-                        <h1>Envio de noticias</h1>
-                    </div>
-                    <div>
-                    <h3>Titulo</h3>
-                        <input type="text" name="texto" id="">
-
-                        <h3>Noticia</h3>
-                        <textarea name="" id="" cols="30" rows="10" class="campoNoticia"></textarea>
-
-                        <button>Enviar</button>
-                    </div>
+            <button class="collapsible square">Historico de noticias</button>
+            <div class="content">
+                <h4>Titulo:</h4>
+                <p>Noticia:</p>
+            </div>
+            <div class="center">
+                <div>
+                    <h1>Envio de noticias</h1>
                 </div>
+                <div>
+                <h3>Titulo:</h3>
+                    <input type="text" name="texto" id="">
+
+                    <h3>Noticia:</h3>
+                    <textarea name="" id="" cols="30" rows="10" class="campoNoticia"></textarea>
+
+                    <button class="button1">Enviar</button>
+                </div>
+            </div>
+
+                <script>
+                    var coll = document.getElementsByClassName("collapsible");
+                    var i;
+
+                    for (i = 0; i < coll.length; i++) {
+                        coll[i].addEventListener("click", function() {
+                        this.classList.toggle("active");
+                        var content = this.nextElementSibling;
+
+                        if (content.style.display === "block") {
+                            content.style.display = "none";
+                        } else {
+                            content.style.display = "block";
+                        }
+                        console.log(content.style.display);
+                    });
+                    }
+                </script>
+
         </body>
     </html>
